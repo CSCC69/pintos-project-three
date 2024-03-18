636a7fafec22fa9c19721535409a4c3b4744fc59
KingLouis-CLXXII <30333055+KingLouis-CLXXII@users.noreply.github.com>
2024-03-18 19:40:01 -0400

    added page funcs

--- src/vm/page.c
+++ src/vm/page.c
@@ -1,3 +1,51 @@
 #include "page.h"
+#include "threads/malloc.h"
 
-*spt_entry create_spt_entry(void *upage,)
+struct spt_entry *create_spt_entry(void *upage, void *kpage, int swap_slot, struct executable_data *executable_data, struct mmap_data *mmap_data, struct hash_elem elem, struct thread *owner){
+    struct spt_entry *spt_entry = malloc(sizeof(struct spt_entry));
+    if (spt_entry == NULL) {
+        return NULL;
+    }
+
+    // Initialize the fields of the struct
+    spt_entry->upage = upage;
+    spt_entry->kpage = kpage;
+    spt_entry->swap_slot = swap_slot;
+    spt_entry->executable_data = executable_data;
+    spt_entry->mmap_data = mmap_data;
+    spt_entry->elem = elem;
+    spt_entry->owner = owner;
+
+    return spt_entry;
+}
+
+struct executable_data *create_executable_data(struct file *file, off_t ofs, uint8_t *upage, uint32_t read_bytes, uint32_t zero_bytes, bool writable){
+    struct executable_data *data = malloc(sizeof(struct executable_data));
+    if (data == NULL) {
+        return NULL;
+    }
+
+    // Initialize the fields of the struct
+    data->file = file;
+    data->ofs = ofs;
+    data->upage = upage;
+    data->read_bytes = read_bytes;
+    data->zero_bytes = zero_bytes;
+    data->writable = writable;
+
+    return data;
+}
+
+struct mmap_data *create_mmap_data(struct file *file, int id, void *addr){
+    struct mmap_data *data = malloc(sizeof(struct mmap_data));
+    if (data == NULL) {
+        return NULL;
+    }
+
+    // Initialize the fields of the struct
+    data->file = file;
+    data->id = id;
+    data->addr = addr;
+
+    return data;
+}
--- src/vm/page.h
+++ src/vm/page.h
@@ -30,3 +30,9 @@ struct mmap_data
     int id;
     void *addr;
   };
+
+struct spt_entry *create_spt_entry(void *upage, void *kpage, int swap_slot, struct executable_data *executable_data, struct mmap_data *mmap_data, struct hash_elem elem, struct thread *owner);
+
+struct executable_data *create_executable_data(struct file *file, off_t ofs, uint8_t *upage, uint32_t read_bytes, uint32_t zero_bytes, bool writable);
+
+struct mmap_data *create_mmap_data(struct file *file, int id, void *addr);
